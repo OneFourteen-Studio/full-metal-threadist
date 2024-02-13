efb49351bd273ad09e15f6b90aa759d1120782ca
OneFourteen Studio <onefourteenstudio114@gmail.com>
2024-02-13 16:22:53 -0500

    linking forms

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -5,6 +5,7 @@ const OrderForm = () => {
     <div className='order-form-container'>
         <h2 className='form-title'>Ready to order?</h2>
         <form name='fileForm' encType='multipart/form-data' data-netlify="true" className='order-form'>
+          <input type='hidden' name='form-name' value='fileForm'/>
           <div className='top-form-container'>
             <input required type='text' name='name' autoComplete='name' className='form-input' placeholder='Full name' />
             <input required type='text' name='email' autoComplete='email' className='form-input' placeholder='Email'/>
